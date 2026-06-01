6c2d3e00a3205c8acfa4da04f3f400d584fcd3f2
caua <caua999999@gmail.com>
2026-05-31 23:52:18 -0300

    novo texto

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,3 +1,15 @@
-test("example", () => {
-    console.log("test Ok!")
+import { sum } from "./server"
+
+describe("sum", () => {
+  test("sum of 3 + 7 must be 10", () => {
+    const result = sum(3, 7)
+
+    expect(result).toBe(10)
+  })
+
+  test("sum of 2 + 2 must be 4", () => {
+    const result = sum(2, 2)
+
+    expect(result).toBe(4)
+  })
 })
