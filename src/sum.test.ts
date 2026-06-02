2d68f6d679177f8901fe77fd56992be17a748f31
caua <caua999999@gmail.com>
2026-06-02 00:00:13 -0300

    Adiciona testes para a função sum

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,10 +1,16 @@
 import { sum } from "./server"
 
 describe("sum", () => {
-  test("sum of 3 + 7 must be 10", () => {
+  let sumResult
+
+  beforeAll(() => {
+    sumResult = 10
+  })
+
+  it("should do sum of 3 + 7 must be 10", () => {
     const result = sum(3, 7)
 
-    expect(result).toBe(10)
+    expect(result).toBe(sumResult)
   })
 
   test("sum of 2 + 2 must be 4", () => {
